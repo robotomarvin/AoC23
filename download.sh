c9d7d92d596fb5cc7cdf789e0eb7886014223127
Martin Bruna <bruna@shoptet.cz>
2024-12-01 12:51:54 +0100

    update tooling

--- download.sh
+++ download.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 
-DAY=$1
-mkdir "d$1"
-cd "d$1"
+DAY=$(date +%d | sed 's/^0*//')
+DAY_PREFIXED=$(date +%d)
+YEAR=$(date +%Y)
+mkdir -p "$YEAR/d$DAY_PREFIXED"
+cd "$YEAR/d$DAY_PREFIXED"
 
-curl --cookie "session=$AOC_SESSION" https://adventofcode.com/2023/day/$1/input > input.txt
-curl --cookie "session=$AOC_SESSION" https://adventofcode.com/2023/day/$1 > instructions.html
+curl --cookie "session=$AOC_SESSION" https://adventofcode.com/$YEAR/day/$DAY/input > input.txt
+curl --cookie "session=$AOC_SESSION" https://adventofcode.com/$YEAR/day/$DAY > instructions.html
 cat instructions.html | xq -x '//pre/code' > test
 cat instructions.html | xq -x '//article' > instructions
 rm instructions.html 
