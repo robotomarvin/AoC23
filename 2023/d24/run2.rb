556650ac26648fefe41c533542df20520f0f7f0a
Martin Bruna <bruna@shoptet.cz>
2024-12-01 12:26:25 +0100

    year split

--- 2023/d24/run2.rb
+++ 2023/d24/run2.rb
@@ -2,7 +2,7 @@ require_relative '../build/z3/lib/z3'
 
 storms = $<.map{|x|
   x.chomp.split(" @ ").map{|y| y.split(", ").map(&:to_i)}
-}
+}[0..2]
 
 solver = Z3::Solver.new
 
@@ -12,9 +12,12 @@ storms.each.with_index do |storm, i|
   p, v = storm
   t = Z3.Real("t#{i}")
   3.times do |j|
-    solver.assert(result_start[j] + t*result_dir[j] == p[j] + t*v[j])
+    exp = result_start[j] + t*result_dir[j] == p[j] + t*v[j]
+    solver.assert(exp)
+    p exp
   end
 end
+p solver
 
 if solver.satisfiable?
   p result_start.map{|x| solver.model[x].to_s.to_i}.sum
